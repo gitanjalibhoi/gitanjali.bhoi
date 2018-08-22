d02a71439955b91db902919671e9f1874e405fbd
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-08-22 17:20:27 +0530

    Add files via upload

---
+++ lab4_6.cpp
@@ -0,0 +1,15 @@
+//enter base and height of a triangle and find its area
+#include<iostream>
+using namespace std;
+int main () {
+  float base,height,area;
+   cout << "enter base and height of a triangle to find its area "<<endl;
+   cout << "input the value of base "<<endl;
+   cout << "input the value of height"<<endl;
+ cin >>  base ;
+ cin >>  height ;
+  area = (base * height)/2 ;
+
+    cout << "the area of the triangle is "<< area <<endl;
+ return 0;
+}                                                          
