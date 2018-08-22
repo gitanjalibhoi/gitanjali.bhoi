14d43f4f30d7f5d51ecf7a23dcd67ff76613c756
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-08-22 17:41:03 +0530

    Add files via upload

---
+++ lab4_7.cpp
@@ -0,0 +1,15 @@
+//calculate area of an equilateral triangle
+#include<iostream>
+using namespace std;
+int main () {
+  float side,area;
+   cout << "calculate the area of an equilateral triangle"<<endl;
+   cout << "its just simple ans,just try hard "<<endl;
+   cout << "please enter the value of side of the equilateral triangle" <<endl;
+ cin >>  side;
+ 
+  area = 1.73/4*(side)*(side);
+
+    cout << "the area of the triangle is "<< area <<endl;
+ return 0;
+}                                                          
