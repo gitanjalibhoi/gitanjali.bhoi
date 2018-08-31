79040e597c612b0adf0ff1ee70bc30ac32866f50
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-08-31 17:59:00 +0530

    Add files via upload

---
+++ lab5_3.cpp
@@ -0,0 +1,22 @@
+//use library
+#include<iostream>
+using namespace std;
+//use main fuction
+int main(){
+//variable declaration
+float a;
+//ask the user for input
+cout<< "enter the number:" <<endl;
+cin >> a;
+//condition
+if (a < 0){
+cout << a <<" is a negative number "<< endl;
+}
+else if (a > 0){
+cout << a << " is a positive number "<<endl;
+}
+else {
+//print the result
+cout << a << "is zero "<<endl;
+  }
+ }
