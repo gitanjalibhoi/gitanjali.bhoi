90ddf466c7ea6bce625a2514dbd64d3b05b18f46
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-08-31 18:42:33 +0530

    Add files via upload

---
+++ lab5_5.cpp
@@ -0,0 +1,19 @@
+//use library
+#include<iostream>
+using namespace std;
+//use main fuction
+int main(){
+//variable declaration
+int a;
+//ask the user for input
+cout<< "enter the number:" <<endl;
+cin >> a;
+//condition
+if (a % 2 == 0){
+cout << a <<" is even number "<< endl;
+}
+else {
+//print the result
+cout << a << " is an odd number" <<endl;
+  }
+ }
