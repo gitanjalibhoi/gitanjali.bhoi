f241a2fb743868ed062aad4ca9b83aec2f6c6cd6
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-09-04 12:43:08 +0530

    Add files via upload

---
+++ lab5_25.cpp
@@ -0,0 +1,15 @@
+//use library
+#include<iostream>
+using namespace std;
+//use main function
+int main (){
+//variable declaration
+ int a = 1;
+//using loop
+while (a < 100 ) {
+if ( a % 2 == 0){
+cout << a << endl ;
+}
+a++;
+}
+ }
