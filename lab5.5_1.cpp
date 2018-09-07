6be1c563a7cf6768ae195c81cafbae7f3bf0e439
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-09-07 19:26:44 +0530

    Add files via upload

--- lab5.5_1.cpp
+++ lab5.5_1.cpp
@@ -6,12 +6,22 @@ using namespace std;
 
 int main(){
     //declaring variables
- int n= 5,i,j;
-   //printing 5 stars for rows
-     for (i=0;i<5;i++){
-//printing stars in 5 columns
- for (j=0;j<5;j++)
-cout<<"*";
-cout<<endl;}
+ int n= 5;
+   //printing star space star
+ for (int i= 0;i< n;i++){ 
+ //printing stars in the first and fifth row
+ if (i==0 or i==(n-1)){
+for (int j=0;j<n;j++){
+ cout<<"*";}}
+//for 2nd,3rd,4th row
+else{
+ //print stars
+ cout<< "*";
+//print spaces
+ for (int j=0;j<(n-2);j++){ 
+ cout<<" ";} 
+ //print stars
+cout<<"*";}
+cout<<endl;}                                      
 return 0;
 }
