ead162fa9a9eade2607c5582f99b949989e12002
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-09-07 17:45:22 +0530

    Add files via upload

---
+++ lab5.5_1.cpp
@@ -0,0 +1,17 @@
+//program to print a square star pattern
+
+//including library
+#include<iostream>
+using namespace std;
+
+int main(){
+    //declaring variables
+ int n= 5,i,j;
+   //printing 5 stars for rows
+     for (i=0;i<5;i++){
+//printing stars in 5 columns
+ for (j=0;j<5;j++)
+cout<<"*";
+cout<<endl;}
+return 0;
+}
