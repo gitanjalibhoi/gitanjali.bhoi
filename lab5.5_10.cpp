4279a0fa36592b8a92bbe3fa1db5d2d37255d4ad
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-09-12 20:20:05 +0530

    Add files via upload

--- lab5.5_10.cpp
+++ lab5.5_10.cpp
@@ -1,15 +1,20 @@
-//mirror right triangle star pattern
+//mirror rhombus star pattern
 #include<iostream>
 using namespace std;
 int main(){
  	int n= 5;
- 	for(int i=0;i<5;i++){						
-		for (int j=0;j<(5-i);j++)		//print spaces
-		cout<<' ';
+ 	for(int i=0;i<5;i++){
+//print space
+       for (int j=0;j<(i+1);j++)				
+		cout<<"  ";
+		
+//print star
+		for (int j=0;j< 5;j++)		
+		cout<<"*";
 
-		for (int j=0;j<i+1;j++)				//print stars
-		cout<<'*';
-
+		
+						
+		
 		cout<< endl;
 	} 
 	return 0;
