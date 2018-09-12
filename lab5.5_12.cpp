63350146f9450598886fb699d53a6e902f9a98a7
gitanjalibhoi <42132644+gitanjalibhoi@users.noreply.github.com>
2018-09-12 15:34:35 +0530

    Add files via upload

---
+++ lab5.5_12.cpp
@@ -0,0 +1,17 @@
+//inverted right triangle star pattern
+#include<iostream>
+using namespace std;
+int main(){
+ int n= 5;
+ for(int i=0;i<5;i++){
+     
+    //print stars
+     int j =0;
+     while (j<(5-i)){
+   cout<<"*";
+j++;
+}
+cout<< endl;}
+return 0;
+}
+
